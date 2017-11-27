83c0e81850a9c733fc614403aec79d99c2e01f72
Raniel <whyfck@gmail.com>
2017-11-27 10:56:13 -0300

    Update supermain.c

--- supermain.c
+++ supermain.c
@@ -4,3 +4,36 @@ main() {
 
 
 }
+
+
+
+/* Raniel */
+
+void iniciaPilha(nomeDaPilha *p) //serve também para listas e outros
+{
+	p->topo= NULL;
+}
+
+void excluirPosicao(Lista *L)
+{
+  printf("Entre com a posição que deseja remover: ");
+  scanf("%d", &remover);
+
+  if (remover > max-1)
+  {
+    printf("Opção inválida!\n");
+  } else
+  {
+    remover--;
+
+    auxMatricula= L-> matricula[remover];
+    auxNota= L-> nota[remover];
+
+    L-> matricula[remover+1]= L-> matricula[remover];
+    L-> nota[remover+1]= L-> nota[remover];
+
+    L-> matricula[L->i]= auxMatricula;
+    L-> nota[L->i]= auxNota;
+
+    L->i--;
+ }
